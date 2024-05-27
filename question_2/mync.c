39021ec30290ff9d1e6cff545bf7bf01c27283c6
noamc <noam12345002@gmail.com>
2024-05-27 19:14:38 +0300

    q3 begining

--- question_2/mync.c
+++ question_2/mync.c
@@ -1,25 +1,17 @@
-// mync -e "ttt 123456789"
-
+//the command ./mync -e "./ttt 123456789"
+#include <sys/types.h> // Add this line
+#include <errno.h>
+#include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-int run_programming(char *input)
-{
+int run_programming(char *input) {
     // Ensure input is not NULL
-    if (input == NULL)
-    {
+    if (input == NULL) {
         fprintf(stderr, "Input string is NULL\n");
-        return 1;
-    }
-
-    // Duplicate the input string to avoid modifying the original
-    char *args_as_string = strdup(input);
-    if (args_as_string == NULL)
-    {
-        perror("strdup");
         return 1;
     }
 
@@ -28,39 +20,43 @@ int run_programming(char *input)
     int argc = 0;
 
     // Tokenize the input string
-    char *token = strtok(args_as_string, " ");
-    while (token != NULL && argc < 19)
-    {
+    char *token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL && argc < 19) {
+        printf("%d",i);
         argv[argc++] = token;
         token = strtok(NULL, " ");
+        i++;
     }
     argv[argc] = NULL; // Null-terminate the array
 
+    // Debug: Print each token to see the result of strtok
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
     // Check if any command was provided
-    if (argc == 0)
-    {
+    if (argc == 0) {
         fprintf(stderr, "Without arguments\n");
-        free(args_as_string);
         return 1;
     }
 
-    // Execute the command
-    execvp(argv[0], argv);
+    // Fork and execute the command
+    int pid;
+    if ((pid = fork()) == -1) {
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        // Child process: execute the command
+        execvp(argv[0], argv);
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+    // Parent process: wait for the child to complete
+    wait(NULL);
 
-    // If execvp returns, there was an error
-    perror("execvp failed");
-    free(args_as_string);
-    return 1;
+    return 0;
 }
-
-
-
-
-
-
-
-
-
 
 int main(int argc, char *argv[])
 {
@@ -76,7 +72,7 @@ int main(int argc, char *argv[])
         switch (opt)
         {
         case 'e':
-        run_programming(optarg);
+            run_programming(optarg);//optarg is the argument after -e
             break;
         default:
             fprintf(stderr, "You should write Usage: %s -e <value>\n", argv[0]);
